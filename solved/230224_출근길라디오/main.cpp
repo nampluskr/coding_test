6aac6d9a561691240ff7de5504146688a0580ddb
nampluskr <nampluskr@gmail.com>
2023-08-01 08:58:52 +0900

    탭 공백으로 변환

--- solved/230224_출근길라디오/main.cpp
+++ solved/230224_출근길라디오/main.cpp
@@ -24,76 +24,76 @@ static int mTime[MAX_N];
 
 static int run()
 {
-	int C;
-	int isOK = 0;
-	int cmd, ret, chk;
-	int N, M;
-	int mID, mTypeID, mNewTime, mRatio;
-	int mA, mB;
+    int C;
+    int isOK = 0;
+    int cmd, ret, chk;
+    int N, M;
+    int mID, mTypeID, mNewTime, mRatio;
+    int mA, mB;
 
-	scanf("%d", &C);
+    scanf("%d", &C);
 
-	for (int c = 0; c < C; ++c)
-	{
-		scanf("%d", &cmd);
-		switch (cmd)
-		{
-		case CMD_INIT:
-			scanf("%d %d ", &N, &M);
-			for (int i = 0; i < N - 1; i++) scanf("%d", &mType[i]);
-			for (int i = 0; i < N - 1; i++) scanf("%d", &mTime[i]);
-			init(N, M, mType, mTime);
-			isOK = 1;
-			break;
+    for (int c = 0; c < C; ++c)
+    {
+        scanf("%d", &cmd);
+        switch (cmd)
+        {
+        case CMD_INIT:
+            scanf("%d %d ", &N, &M);
+            for (int i = 0; i < N - 1; i++) scanf("%d", &mType[i]);
+            for (int i = 0; i < N - 1; i++) scanf("%d", &mTime[i]);
+            init(N, M, mType, mTime);
+            isOK = 1;
+            break;
 
-		case CMD_UPDATE:
-			scanf("%d %d", &mID, &mNewTime);
-			update(mID, mNewTime);
-			break;
+        case CMD_UPDATE:
+            scanf("%d %d", &mID, &mNewTime);
+            update(mID, mNewTime);
+            break;
 
-		case CMD_UPDATE_TYPE:
-			scanf("%d %d", &mTypeID, &mRatio);
-			ret = updateByType(mTypeID, mRatio);
-			scanf("%d", &chk);
-			if (ret != chk) {
-				isOK = 0;
-			}
-			break;
+        case CMD_UPDATE_TYPE:
+            scanf("%d %d", &mTypeID, &mRatio);
+            ret = updateByType(mTypeID, mRatio);
+            scanf("%d", &chk);
+            if (ret != chk) {
+                isOK = 0;
+            }
+            break;
 
-		case CMD_CALC:
-			scanf("%d %d", &mA, &mB);
-			ret = calculate(mA, mB);
-			scanf("%d", &chk);
-			if (ret != chk) {
-				isOK = 0;
-			}
-			break;
+        case CMD_CALC:
+            scanf("%d %d", &mA, &mB);
+            ret = calculate(mA, mB);
+            scanf("%d", &chk);
+            if (ret != chk) {
+                isOK = 0;
+            }
+            break;
 
-		default:
-			isOK = 0;
-			break;
-		}
-	}
-	destroy();
-	return isOK;
+        default:
+            isOK = 0;
+            break;
+        }
+    }
+    destroy();
+    return isOK;
 }
 
 int main()
 {
-	clock_t start = clock();
-	setbuf(stdout, NULL);
-	freopen("sample_input.txt", "r", stdin);
+    clock_t start = clock();
+    setbuf(stdout, NULL);
+    freopen("sample_input.txt", "r", stdin);
 
-	int T, MARK;
-	scanf("%d %d", &T, &MARK);
+    int T, MARK;
+    scanf("%d %d", &T, &MARK);
 
-	for (int tc = 1; tc <= T; tc++)
-	{
-		if (run()) printf("#%d %d\n", tc, MARK);
-		else printf("#%d %d\n", tc, 0);
-	}
-	int result = (clock() - start) / (CLOCKS_PER_SEC / 1000);
-	printf(">> Result: %d ms\n", result);
+    for (int tc = 1; tc <= T; tc++)
+    {
+        if (run()) printf("#%d %d\n", tc, MARK);
+        else printf("#%d %d\n", tc, 0);
+    }
+    int result = (clock() - start) / (CLOCKS_PER_SEC / 1000);
+    printf(">> Result: %d ms\n", result);
 
-	return 0;
+    return 0;
 }
--- solved/230429_블록쌓기게임/main.cpp
+++ solved/230429_블록쌓기게임/main.cpp
@@ -7,8 +7,8 @@
 
 struct Result
 {
-	int top;
-	int count;
+    int top;
+    int count;
 };
 
 void init(int C);
@@ -19,58 +19,58 @@ Result dropBlocks(int mCol, int mHeight, int mLength);
 
 static bool run()
 {
-	int query_num;
-	scanf("%d", &query_num);
-	int ans_top, ans_count;
-	bool ok = false;
+    int query_num;
+    scanf("%d", &query_num);
+    int ans_top, ans_count;
+    bool ok = false;
 
-	for (int q = 0; q < query_num; q++)
-	{
-		int query;
-		scanf("%d", &query);
+    for (int q = 0; q < query_num; q++)
+    {
+        int query;
+        scanf("%d", &query);
 
-		if (query == CMD_INIT)
-		{
-			int C;
-			scanf("%d", &C);
-			init(C);
-			ok = true;
-		}
+        if (query == CMD_INIT)
+        {
+            int C;
+            scanf("%d", &C);
+            init(C);
+            ok = true;
+        }
 
-		else if (query == CMD_DROP)
-		{
-			int mCol, mHeight, mLength;
-			scanf("%d %d %d", &mCol, &mHeight, &mLength);
-			Result ret = dropBlocks(mCol, mHeight, mLength);
-			scanf("%d %d", &ans_top, &ans_count);
+        else if (query == CMD_DROP)
+        {
+            int mCol, mHeight, mLength;
+            scanf("%d %d %d", &mCol, &mHeight, &mLength);
+            Result ret = dropBlocks(mCol, mHeight, mLength);
+            scanf("%d %d", &ans_top, &ans_count);
 
-			if (ans_top != ret.top || ans_count != ret.count)
-			{
-				ok = false;
-			}
-		}
-	}
-	return ok;
+            if (ans_top != ret.top || ans_count != ret.count)
+            {
+                ok = false;
+            }
+        }
+    }
+    return ok;
 }
 
 int main()
 {
-	clock_t start = clock();
-	setbuf(stdout, NULL);
-	freopen("sample_input.txt", "r", stdin);
+    clock_t start = clock();
+    setbuf(stdout, NULL);
+    freopen("sample_input.txt", "r", stdin);
 
-	int T, MARK;
-	scanf("%d %d", &T, &MARK);
+    int T, MARK;
+    scanf("%d %d", &T, &MARK);
 
-	//T = 5;
-	for (int tc = 1; tc <= T; tc++)
-	{
-		clock_t tc_start = clock();
-		int score = run() ? MARK : 0;
-		int tc_result = (clock() - tc_start) / (CLOCKS_PER_SEC / 1000);
-		printf("#%2d %d (%3d ms)\n", tc, score, tc_result);
-	}
-	int result = (clock() - start) / (CLOCKS_PER_SEC / 1000);
-	printf(">> Result: %d ms\n", result);
-	return 0;
+    //T = 5;
+    for (int tc = 1; tc <= T; tc++)
+    {
+        clock_t tc_start = clock();
+        int score = run() ? MARK : 0;
+        int tc_result = (clock() - tc_start) / (CLOCKS_PER_SEC / 1000);
+        printf("#%2d %d (%3d ms)\n", tc, score, tc_result);
+    }
+    int result = (clock() - start) / (CLOCKS_PER_SEC / 1000);
+    printf(">> Result: %d ms\n", result);
+    return 0;
 }
