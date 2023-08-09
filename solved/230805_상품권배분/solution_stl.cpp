849d1cc73769e4ffa5517d91d429409fd26fe35e
nampluskr <37031766+nampluskr@users.noreply.github.com>
2023-08-09 09:46:48 +0900

    Update solution_stl.cpp

--- solved/230805_상품권배분/solution_stl.cpp
+++ solved/230805_상품권배분/solution_stl.cpp
@@ -1,9 +1,11 @@
-﻿#if 1
+#if 0
+// STL 421 ms (WS)
 #include <vector>
 #include <unordered_map>
 using namespace std;
 
 #define NUM_NODES   (17000 + 1000)
+#define NUM_GROPUS  1000
 #define DELETED     1
 
 // 1 ≤ mId ≤ 1,000,000,000
@@ -20,6 +22,9 @@ struct Node {
 unordered_map<int, int> nodeMap;
 vector<Node> nodes;
 int nodeCnt;
+
+vector<int> groups;
+int groupCnt;
 
 //////////////////////////////////////////////////////////////////////////////
 int get_nodeIndex(int mId) {
@@ -55,6 +60,9 @@ void init(int N, int mId[], int mNum[])
     nodeMap.clear();
     nodes.clear();  nodes.resize(NUM_NODES);
     nodeCnt = 0;
+
+    groups.clear(); groups.resize(NUM_GROPUS);
+    groupCnt = N;
 
     for (int i = 0; i < N; i++) {
         int nIdx = get_nodeIndex(mId[i]);
@@ -104,6 +112,25 @@ int remove(int mId)
 // 1,000
 int distribute(int K)
 {
-    return 0;
+    int ret;
+    int start = 0;
+    int end = 0;
+    for (int i = 0; i < groupCnt; i++) {
+        groups[i] = nodes[i].num;
+        end = max(end, groups[i]);
+    }
+    // Parametric search
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+        int sum = 0;
+        for (int i = 0; i < groupCnt; i++) { sum += min(mid, groups[i]); }
+
+        if (sum <= K) {
+            ret = mid;
+            start = mid + 1;
+        }
+        else { end = mid - 1; }
+    }
+    return ret;
 }
 #endif
