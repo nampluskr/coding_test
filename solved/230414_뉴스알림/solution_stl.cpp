f9f0249b175a5d7a2055597a8b5474d22b4592b0
nampluskr <nampluskr@gmail.com>
2023-08-17 16:44:33 +0900

    업데이트

---
+++ solved/230414_뉴스알림/solution_stl.cpp
@@ -0,0 +1,30 @@
+#if 1
+#include <vector>
+using namespace std;
+
+//////////////////////////////////////////////////////////////////////////////
+void init(int N, int K)
+{
+
+}
+
+void registerUser(int mTime, int mUID, int mNum, int mChannelIDs[])
+{
+
+}
+
+int offerNews(int mTime, int mNewsID, int mDelay, int mChannelID)
+{
+	return -1;
+}
+
+void cancelNews(int mTime, int mNewsID)
+{
+
+}
+
+int checkUser(int mTime, int mUID, int mRetIDs[])
+{
+	return -1;
+}
+#endif
