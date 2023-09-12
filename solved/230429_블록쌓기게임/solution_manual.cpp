02bcec9b8bfca98e3e5317a7a2809a98f52ad7eb
nampluskr <nampluskr@gmail.com>
2023-09-12 23:45:23 +0900

    update

--- solved/230429_블록쌓기게임/solution_manual.cpp
+++ solved/230429_블록쌓기게임/solution_manual.cpp
@@ -1,48 +1,49 @@
-﻿#if 0
-// Manual 871 ms: Partion (Range update + total query)
-// Array
-#define NUM_VALUES      1000000
-#define BUCKET_SIZE     1000        // sqrt(NUM_VALUES)
-#define NUM_BUCKETS     1000        // ceil((double)NUM_VALUES / BUCKET_SIZE)
-#define INT32_MAX       2147483647    // INT32_MAX
+﻿#if 1
+// Manual 920 ms: Partion (Range update + total query)
+#include <cmath>
+
+#define MAX_VALUES      1000000
+#define MAX_BUCKETS     1000
+#define INF             10000
 
 inline int min(int a, int b) { return (a < b) ? a : b; }
 inline int max(int a, int b) { return (a > b) ? a : b; }
 
 struct Result {
-    int top;    // 가장 높은 블록의 높이
-    int count;    // 남아 있는 블록들의 개수를 1,000,000으로 나눈 나머지
+    int top;
+    int count;
 };
+
+int num_values;
+int num_buckets;
+int bucket_size;
 
 struct Data {
     int top;
-    int bottom;
+    int bot;
+    long long sum;
     int base;
-    long long count;
 };
 
 struct Partition {
-    Data buckets[NUM_BUCKETS];
-    int values[NUM_VALUES];
+    Data buckets[MAX_BUCKETS];
+    int values[MAX_VALUES];
     int N;                // bucket size
-    int columnCnt;        // number of columns
 
-    void init(int num_columns) {
-        N = BUCKET_SIZE;
-        columnCnt = num_columns;
-        for (int i = 0; i < columnCnt; i++) { values[i] = 0; }
-        for (int i = 0; i < NUM_BUCKETS; i++) { buckets[i] = { 0, 0, 0, 0 }; }
+    void init() {
+        N = bucket_size;
+        for (int i = 0; i < num_values; i++) { values[i] = 0; }
+        for (int i = 0; i < num_buckets; i++) { buckets[i] = {}; }
     }
-    void update_block(int bIdx) {
+    void update_minmax(int bIdx) {
         int left = bIdx * N;
-        int right = min((bIdx + 1) * N - 1, columnCnt - 1);
+        int right = min((bIdx + 1) * N - 1, num_values - 1);
 
         buckets[bIdx].top = 0;
-        buckets[bIdx].bottom = INT32_MAX;
-
+        buckets[bIdx].bot = INF;
         for (int i = left; i <= right; i++) {
-            buckets[bIdx].top = max(buckets[bIdx].top, values[i]);
-            buckets[bIdx].bottom = min(buckets[bIdx].bottom, values[i]);
+            buckets[bIdx].top = max(buckets[bIdx].top, query(i));
+            buckets[bIdx].bot = min(buckets[bIdx].bot, query(i));
         }
     }
     void update(int left, int right, int value) {
@@ -52,44 +53,42 @@ struct Partition {
         if (s == e) {
             for (int i = left; i <= right; i++) {
                 values[i] += value;
-                buckets[i / N].count += value;
+                buckets[s].sum += value;
             }
-            update_block(s);
+            update_minmax(s);
             return;
         }
         while (left / N == s) {
-            values[left] += value;
-            buckets[left / N].count += value;
-            left += 1;
+            values[left++] += value;
+            buckets[s].sum += value;
         }
+        update_minmax(s);
         while (right / N == e) {
-            values[right] += value;
-            buckets[right / N].count += value;
-            right -= 1;
+            values[right--] += value;
+            buckets[e].sum += value;
         }
-        update_block(s);
-        update_block(e);
+        update_minmax(e);
         for (int i = s + 1; i <= e - 1; i++) {
-            buckets[i].count += value * N;
+            buckets[i].top += value;
+            buckets[i].bot += value;
+            buckets[i].sum += value * N;
             buckets[i].base += value;
         }
     }
-    Result query() {
-        Result ret{ 0, 0 };    // top, count
-        int top = 0;
-        int bottom = INT32_MAX;
-        long long count = 0;
+    int query(int idx) {
+        return values[idx] + buckets[idx / N].base;
+    }
+    Data query() {
+        Data data = { 0, INF, 0 };
 
-        for (int i = 0; i < NUM_BUCKETS; i++) {
-            if (buckets[i].count > 0) {
-                top = max(top, buckets[i].top + buckets[i].base);
-                bottom = min(bottom, buckets[i].bottom + buckets[i].base);
-                count += buckets[i].count;
+        for (int i = 0; i < num_buckets; i++) {
+            if (buckets[i].sum > 0) {
+                data.top = max(data.top, buckets[i].top);
+                data.bot = min(data.bot, buckets[i].bot);
+                data.sum += buckets[i].sum;
             }
         }
-        ret.top = top - bottom;
-        ret.count = (count - (long long)columnCnt * bottom) % 1'000'000;
-        return ret;
+        return data;
     }
 };
 Partition part;
@@ -97,16 +96,22 @@ Partition part;
 /////////////////////////////////////////////////////////////////////
 void init(int C)
 {
-    part.init(C);
+    bucket_size = sqrt(C);
+    num_values = C;
+    num_buckets = ceil((double)num_values / bucket_size);
+    part.init();
 }
 
 // 3,000
 Result dropBlocks(int mCol, int mHeight, int mLength)
 {
-    Result ret{ 0, 0 };    // top, count
+    Result ret{ 0, 0 };    // top, sum
 
     part.update(mCol, mCol + mLength - 1, mHeight);
-    ret = part.query();
+    Data data = part.query();
+
+    ret.top = data.top - data.bot;
+    ret.count = (data.sum - (long long)num_values * data.bot) % 1000000;
     return ret;
 }
 #endif
--- solved/230429_블록쌓기게임/solution_stl.cpp
+++ solved/230429_블록쌓기게임/solution_stl.cpp
@@ -1,38 +1,48 @@
-﻿// STL 1728 ms: Partion (Range update + total query)
+﻿#if 0
+// STL 1728 ms: Partion (Range update + total query)
 #include <vector>
+#include <cmath>
 using namespace std;
 
+#define INF 10000
+
+inline int min(int a, int b) { return (a < b) ? a : b; }
+inline int max(int a, int b) { return (a > b) ? a : b; }
+
 struct Result {
-    int top;    // 가장 높은 블록의 높이
-    int count;    // 남아 있는 블록들의 개수를 1,000,000으로 나눈 나머지
+    int top;
+    int count;
 };
+
+int num_values;
+int num_buckets;
+int bucket_size;
+
 struct Data {
     int top;
-    int bottom;
+    int bot;
+    long long sum;
     int base;
-    long long count;
 };
 struct Partition {
     vector<Data> buckets;
     vector<int> values;
-    int N;        // bucket size
+    int N;          // bucket size
 
-    void init(int num_values) {
-        N = sqrt(num_values);
-        int num_buckets = ceil((double)num_values / N);
+    void init() {
+        N = bucket_size;
         buckets.clear();    buckets.resize(num_buckets);
-        values.clear();        values.resize(num_values);
+        values.clear();     values.resize(num_values);
     }
-    void update_block(int bIdx) {
+    void update_minmax(int bIdx) {
         int left = bIdx * N;
-        int right = min((bIdx + 1) * N - 1, (int)values.size() - 1);
+        int right = min((bIdx + 1) * N - 1, num_values - 1);
 
         buckets[bIdx].top = 0;
-        buckets[bIdx].bottom = INT32_MAX;
-
+        buckets[bIdx].bot = INF;
         for (int i = left; i <= right; i++) {
-            buckets[bIdx].top = max(buckets[bIdx].top, values[i]);
-            buckets[bIdx].bottom = min(buckets[bIdx].bottom, values[i]);
+            buckets[bIdx].top = max(buckets[bIdx].top, query(i));
+            buckets[bIdx].bot = min(buckets[bIdx].bot, query(i));
         }
     }
     void update(int left, int right, int value) {
@@ -42,58 +52,64 @@ struct Partition {
         if (s == e) {
             for (int i = left; i <= right; i++) {
                 values[i] += value;
-                buckets[i / N].count += value;
+                buckets[s].sum += value;
             }
-            update_block(s);
+            update_minmax(s);
             return;
         }
         while (left / N == s) {
-            values[left] += value;
-            buckets[left / N].count += value;
-            left += 1;
+            values[left++] += value;
+            buckets[s].sum += value;
         }
+        update_minmax(s);
         while (right / N == e) {
-            values[right] += value;
-            buckets[right / N].count += value;
-            right -= 1;
+            values[right--] += value;
+            buckets[e].sum += value;
         }
-        update_block(s);
-        update_block(e);
+        update_minmax(e);
         for (int i = s + 1; i <= e - 1; i++) {
-            buckets[i].count += value * N;
+            buckets[i].top += value;
+            buckets[i].bot += value;
+            buckets[i].sum += value * N;
             buckets[i].base += value;
         }
     }
-    Result query() {
-        Result ret{ 0, 0 };    // top, count
-        int top = 0;
-        int bottom = INT32_MAX;
-        long long count = 0;
+    int query(int idx) {
+        return values[idx] + buckets[idx / N].base;
+    }
+    Data query() {
+        Data ret = { 0, INF, 0 };
 
-        for (int i = 0; i < buckets.size(); i++) {
-            top = max(top, buckets[i].top + buckets[i].base);
-            bottom = min(bottom, buckets[i].bottom + buckets[i].base);
-            count += buckets[i].count;
+        for (int i = 0; i < num_buckets; i++) {
+            ret.top = max(ret.top, buckets[i].top);
+            ret.bot = min(ret.bot, buckets[i].bot);
+            ret.sum += buckets[i].sum;
         }
-        ret.top = top - bottom;
-        ret.count = (count - (long long)values.size() * bottom) % 1'000'000;
         return ret;
     }
 };
 Partition part;
 
-/////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
+// C: 격자판의 열의 개수 (10 ≤ C ≤ 1,000,000)
 void init(int C)
 {
-    part.init(C);
+    bucket_size = sqrt(C);
+    num_values = C;
+    num_buckets = ceil((double)num_values / bucket_size);
+    part.init();
 }
 
 // 3,000
 Result dropBlocks(int mCol, int mHeight, int mLength)
 {
-    Result ret{ 0, 0 };    // top, count
+    Result ret = { 0, 0 };
 
     part.update(mCol, mCol + mLength - 1, mHeight);
-    ret = part.query();
+    Data data = part.query();
+
+    ret.top = data.top - data.bot;
+    ret.count = (data.sum - (long long)num_values * data.bot) % 1000000;
     return ret;
 }
+#endif
