c47f94bf95e9944ae6dafa1e8fc349d2bc1c2579
nampluskr <37031766+nampluskr@users.noreply.github.com>
2023-09-19 15:29:40 +0900

    Update solution_stl.cpp

--- solved/230624_단어미로/solution_stl.cpp
+++ solved/230624_단어미로/solution_stl.cpp
@@ -1,88 +1,83 @@
 #if 0
-// STL 1742 ms: 
 #include <vector>
 #include <string>
-#include <unordered_map>
 #include <queue>
+#include <unordered_map>
 using namespace std;
 
-#define LEN_WORD    (11 + 1)
-#define NUM_ROOMS   (30000 + 1)
+#define MAX_ROOMS	(30000 + 1)
 
 struct Room {
     int mID;
     string word;
-    string dir[3];      // 접두어, 가운데, 접미어
-
-    bool operator<(const Room& room) const { return word > room.word; }
+    string dir[3];
 };
-unordered_map<string, int> roomMap;
 vector<Room> rooms;
-int current_mID;
+unordered_map<string, int> roomMap;
+int current_roomID;
 
-unordered_map<string, priority_queue<Room>> dirMap[3];
+struct Data {
+    int mID;
+    string word;
+    bool operator<(const Data& data) const { return word > data.word; }
+};
+unordered_map<string, priority_queue<Data>> dirPQMap[3];
 
-//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
 void init()
 {
-    rooms.clear();  rooms.resize(NUM_ROOMS);
+    rooms.clear();	rooms.resize(MAX_ROOMS);
     roomMap.clear();
 
-    for (int i = 0; i < 3; i++) { dirMap[i].clear(); }
+    for (int i = 0; i < 3; i++) { dirPQMap[i].clear(); }
 }
 
-// 30,000
-// mID는 맨 처음 호출할 때 1이고 그 다음 호출할 때마다 1씩 증가한다.
 void addRoom(int mID, char mWord[], int mDirLen[])
 {
     string word = string(mWord);
     rooms[mID].mID = mID;
     rooms[mID].word = word;
-    rooms[mID].dir[0] = word.substr(0, mDirLen[0]);                 // 앞 방향
-    rooms[mID].dir[1] = word.substr(4, mDirLen[1]);                 // 중간 방향
-    rooms[mID].dir[2] = word.substr(11 - mDirLen[2], mDirLen[2]);   // 뒤 방향
+    rooms[mID].dir[0] = word.substr(0, mDirLen[0]);
+    rooms[mID].dir[1] = word.substr(4, mDirLen[1]);
+    rooms[mID].dir[2] = word.substr(11 - mDirLen[2], mDirLen[2]);
 
-    roomMap[string(mWord)] = mID;
+    roomMap[word] = mID;
 
-    dirMap[0][word.substr(9, 2)].push({ mID, word });   // 접미어 -> 앞 방향
-    dirMap[0][word.substr(7, 4)].push({ mID, word });   // 접미어 -> 앞 방향
-    dirMap[1][word.substr(4, 3)].push({ mID, word });   // 가운데 -> 중간 방향
-    dirMap[2][word.substr(0, 2)].push({ mID, word });   // 접두어 -> 뒤 방향
-    dirMap[2][word.substr(0, 4)].push({ mID, word });   // 접두어 -> 뒤 방향
+    dirPQMap[0][word.substr(9, 2)].push({ mID, word });
+    dirPQMap[0][word.substr(7, 4)].push({ mID, word });
+    dirPQMap[1][word.substr(4, 3)].push({ mID, word });
+    dirPQMap[2][word.substr(0, 2)].push({ mID, word });
+    dirPQMap[2][word.substr(0, 4)].push({ mID, word });
 }
 
-// 500
 void setCurrent(char mWord[])
 {
-    current_mID = roomMap[string(mWord)];
+    current_roomID = roomMap[string(mWord)];
 }
 
-// 50,000
-// 사전 순으로 가장 빠른 단어가 있는 방으로 이동
 int moveDir(int mDir)
 {
-    string word = rooms[current_mID].dir[mDir];
-    priority_queue<Room>& Q = dirMap[mDir][word];
-    vector<Room> popped;
+    string subWord = rooms[current_roomID].dir[mDir];
+    auto& Q = dirPQMap[mDir][subWord];
+
     int ret = 0;
-
+    vector<int> popped;
     while (!Q.empty()) {
-        Room room = Q.top(); Q.pop();
+        auto data = Q.top(); Q.pop();
 
-        if (rooms[room.mID].word != room.word) continue;
+        if (rooms[data.mID].word != data.word) continue;
 
-        popped.push_back(room);
-        if (room.mID != current_mID) {
-            ret = room.mID;
+        popped.push_back(data.mID);
+        if (data.mID != current_roomID) {
+            ret = data.mID;
             break;
         }
     }
-    for (const auto& room : popped) { Q.push(room); }
-    if (ret != 0) { current_mID = ret; }
+    for (int mID : popped) { Q.push({ mID, rooms[mID].word }); }
+    if (ret != 0) { current_roomID = ret; }
     return ret;
 }
 
-// 30,000
 void changeWord(char mWord[], char mChgWord[], int mChgLen[])
 {
     int mID = roomMap[string(mWord)];
