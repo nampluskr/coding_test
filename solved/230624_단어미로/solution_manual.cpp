f714118af46a5463435b2df492f0fe8557938f05
nampluskr <37031766+nampluskr@users.noreply.github.com>
2023-09-19 15:30:11 +0900

    Update solution_manual.cpp

--- solved/230624_단어미로/solution_manual.cpp
+++ solved/230624_단어미로/solution_manual.cpp
@@ -1,70 +1,104 @@
 #if 1
-// Manual 769 ms: 
-// vector clear(), resize() -> ¹è¿­ / ÃÊ±âÈ­
-// vector -> Linked List
-// ¹®ÀÚ¿­ ºñ±³: strcpy(), strcmp()
-// priority_queue -> manual pq
-
+#ifndef _CRT_SECURE_NO_WARNINGS
 #define _CRT_SECURE_NO_WARNINGS
+#endif
 
 #include <vector>
-#include <string>
-#include <unordered_map>
-#include <queue>
+#include <cstring>
 using namespace std;
 
-#define LEN_WORD    (11 + 1)
-#define NUM_ROOMS   (30000 + 1)
+#define MAX_ROOMS	(30000 + 1)
+#define MAX_WORD    (11 + 1)
+
+template<typename Type>
+struct LinkedList {
+    struct ListNode {
+        Type data;
+        ListNode* next;
+    };
+    ListNode* head;
+    ListNode* tail;
+
+    void clear() { head = nullptr; tail = nullptr; }
+    void push_back(const Type& data) {
+        ListNode* node = new ListNode({ data, nullptr });
+        if (head == nullptr) { head = node; tail = node; }
+        else { tail->next = node; tail = node; }
+    }
+};
+
+# define MAX_TABLE 30001
+struct HashMap {
+    struct MapData {
+        char key[MAX_WORD];
+        int value;
+
+        MapData() { strcpy_s(this->key, ""); this->value = 0; }
+        MapData(const char key[], int value) { strcpy_s(this->key, key); this->value = value; }
+    };
+    LinkedList<MapData> table[MAX_TABLE];
+
+    int hash(const char* str) {
+        unsigned long hash = 5381;
+        int c;
+        while (c = *str++) {
+            hash = (((hash << 5) + hash) + c) % MAX_TABLE;
+        }
+        return hash % MAX_TABLE;
+    }
+    void clear() { for (int i = 0; i < MAX_TABLE; i++) table[i].clear(); }
+    int find(const char key[]) {
+        int hashkey = hash(key);
+        for (auto node = table[hashkey].head; node; node = node->next) {
+            if (strcmp(node->data.key, key) == 0)
+                return node->data.value;
+        }
+        return -1;
+    }
+    int operator[](const char key[]) {
+        return find(key);
+    }
+    void emplace(const char key[], int value) {
+        int hashkey = hash(key);
+        table[hashkey].push_back({ key, value });
+    }
+};
 
 struct Room {
     int mID;
-    char word[LEN_WORD];
-    string dir[3];      // Á¢µÎ¾î, °¡¿îµ¥, Á¢¹Ì¾î
-
-    Room() {
-        mID = 0;
-        strcpy(word, "");
-        for (int i = 0; i < 3; i++) { dir[i].clear(); }
-    }
+    char mWord[MAX_WORD];
+    char dir[3][MAX_WORD];
 };
-unordered_map<string, int> roomMap;
-Room rooms[NUM_ROOMS];
-int current_mID;
+Room rooms[MAX_ROOMS];
+HashMap roomMap;
+int current_roomID;
 
 struct Data {
     int mID;
-    char word[LEN_WORD];
+    char mWord[MAX_WORD];
 
-    Data() { mID = 0; strcpy(word, ""); }
-    Data(int _mID, char _word[]) { mID = _mID; strcpy(word, _word); }
-    bool operator<(const Data& room) const { return strcmp(word, room.word) > 0; }
+    Data() { this->mID = 0; strcpy_s(this->mWord, ""); }    // ì—†ìœ¼ë©´ C2280 ì—ëŸ¬
+    Data(int mID, const char mWord[]) { this->mID = mID; strcpy_s(this->mWord, mWord); }
+    bool operator<(const Data& data) const { return strcmp(mWord, data.mWord) > 0; }
 };
 
-#if 1
+template<typename Type>
 struct PriorityQueue {
-    priority_queue<Data> heap;
+    vector<Type> heap;
+    //Type heap[10000];     // ë©”ëª¨ë¦¬ ì´ˆê³¼
+    int heapSize;
 
-    void init() { while (!heap.empty()) { heap.pop(); } }
-    void push(const Data& data) { heap.push(data); }
-    void pop() { heap.pop(); }
-    Data top() { return heap.top(); }
-    bool empty() { return heap.empty(); }
-};
-#else
-struct PriorityQueue {
-    vector<Data> heap;
-    int heapSize = 0;
-
-    void init() { heap.clear(); heapSize = 0; }
-    void push(const Data& data) {
-        if (heapSize >= heap.size())
-            heap.push_back({});
+    void clear() { 
+        heap.clear(); 
+        heapSize = 0; }
+    void push(const Type& data) {
+        if (heapSize >= heap.size()) { heap.push_back({}); }
 
         heap[heapSize] = data;
         int current = heapSize;
-
-        while (current > 0 && heap[(current - 1) / 2] < heap[current]) {
-            Data temp = heap[(current - 1) / 2];
+        while (current > 0 && heap[(current - 1) / 2] < heap[current])
+        {
+            Type temp = heap[(current - 1) / 2];
             heap[(current - 1) / 2] = heap[current];
             heap[current] = temp;
             current = (current - 1) / 2;
@@ -78,109 +112,109 @@ struct PriorityQueue {
 
         while (current * 2 + 1 < heapSize) {
             int child;
-            if (current * 2 + 2 == heapSize)
+            if (current * 2 + 2 == heapSize) {
                 child = current * 2 + 1;
-            else
+            }
+            else {
                 child = heap[current * 2 + 2] < heap[current * 2 + 1] ? current * 2 + 1 : current * 2 + 2;
-
-            if (heap[child] < heap[current])
+            }
+            if (heap[child] < heap[current]) {
                 break;
-
-            Data temp = heap[current];
+            }
+            Type temp = heap[current];
             heap[current] = heap[child];
             heap[child] = temp;
             current = child;
         }
     }
-    Data top() { return heap[0]; }
     bool empty() { return heapSize == 0; }
+    Type top() { return heap[0]; }
 };
-#endif
-unordered_map<string, PriorityQueue> dirMap[3];
 
-struct LinkedList {
-    struct Node {
-        int value;
-        Node* next;
-    };
-    Node* head;
-    Node* tail;
+HashMap dirMap[3];
+PriorityQueue<Data> dirPQ[3][MAX_ROOMS];
+int dirMapCnt[3];
 
-    void init() { head = nullptr; tail = nullptr; }
-    void push_back(int value) {
-        Node* node = new Node({ value, nullptr });
-        if (head == nullptr) { head = node; tail = node; }
-        else { tail->next = node; tail = node; }
+int get_mapIndex(int mDir, const char subWord[]) {
+    int mIdx;
+    auto ret = dirMap[mDir].find(subWord);
+    if (ret == -1) {
+        mIdx = dirMapCnt[mDir]++;
+        dirMap[mDir].emplace(subWord, mIdx);
     }
-};
+    else { mIdx = ret; }
+    return mIdx;
+}
 
-//////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////
 void init()
 {
-    for (int i = 0; i < NUM_ROOMS; i++) { rooms[i] = Room(); }
+    for (int i = 0; i < MAX_ROOMS; i++) { rooms[i] = {}; }
     roomMap.clear();
 
-    for (int i = 0; i < 3; i++) { dirMap[i].clear(); }
+    for (int i = 0; i < 3; i++) {
+        dirMap[i].clear();
+        dirMapCnt[i] = 0;
+        for (int j = 0; j < MAX_ROOMS; j++)
+            dirPQ[i][j].clear();
+    }
 }
 
-// 30,000
-// mID´Â ¸Ç Ã³À½ È£ÃâÇÒ ¶§ 1ÀÌ°í ±× ´ÙÀ½ È£ÃâÇÒ ¶§¸¶´Ù 1¾¿ Áõ°¡ÇÑ´Ù.
 void addRoom(int mID, char mWord[], int mDirLen[])
 {
-    string word = string(mWord);
     rooms[mID].mID = mID;
-    strcpy(rooms[mID].word, mWord);
-    rooms[mID].dir[0] = word.substr(0, mDirLen[0]);                 // ¾Õ ¹æÇâ
-    rooms[mID].dir[1] = word.substr(4, mDirLen[1]);                 // Áß°£ ¹æÇâ
-    rooms[mID].dir[2] = word.substr(11 - mDirLen[2], mDirLen[2]);   // µÚ ¹æÇâ
+    strcpy_s(rooms[mID].mWord, mWord);
+    strncpy_s(rooms[mID].dir[0], mWord, mDirLen[0]);
+    strncpy_s(rooms[mID].dir[1], mWord + 4, mDirLen[1]);
+    strncpy_s(rooms[mID].dir[2], mWord + 11 - mDirLen[2], mDirLen[2]);
 
-    roomMap[string(mWord)] = mID;
+    roomMap.emplace(mWord, mID);
 
-    dirMap[0][word.substr(9, 2)].push({ mID, mWord });   // Á¢¹Ì¾î -> ¾Õ ¹æÇâ
-    dirMap[0][word.substr(7, 4)].push({ mID, mWord });   // Á¢¹Ì¾î -> ¾Õ ¹æÇâ
-    dirMap[1][word.substr(4, 3)].push({ mID, mWord });   // °¡¿îµ¥ -> Áß°£ ¹æÇâ
-    dirMap[2][word.substr(0, 2)].push({ mID, mWord });   // Á¢µÎ¾î -> µÚ ¹æÇâ
-    dirMap[2][word.substr(0, 4)].push({ mID, mWord });   // Á¢µÎ¾î -> µÚ ¹æÇâ
+    int mIdx;
+    char subWord[5];
+    strncpy_s(subWord, mWord + 9, 2);  mIdx = get_mapIndex(0, subWord); dirPQ[0][mIdx].push({ mID, mWord });
+    strncpy_s(subWord, mWord + 7, 4);  mIdx = get_mapIndex(0, subWord); dirPQ[0][mIdx].push({ mID, mWord });
+    strncpy_s(subWord, mWord + 4, 3);  mIdx = get_mapIndex(1, subWord); dirPQ[1][mIdx].push({ mID, mWord });
+    strncpy_s(subWord, mWord + 0, 2);  mIdx = get_mapIndex(2, subWord); dirPQ[2][mIdx].push({ mID, mWord });
+    strncpy_s(subWord, mWord + 0, 4);  mIdx = get_mapIndex(2, subWord); dirPQ[2][mIdx].push({ mID, mWord });
 }
 
-// 500
 void setCurrent(char mWord[])
 {
-    current_mID = roomMap[string(mWord)];
+    current_roomID = roomMap[mWord];
 }
 
-// 50,000
-// »çÀü ¼øÀ¸·Î °¡Àå ºü¸¥ ´Ü¾î°¡ ÀÖ´Â ¹æÀ¸·Î ÀÌµ¿
 int moveDir(int mDir)
 {
-    string word = rooms[current_mID].dir[mDir];
-    PriorityQueue& Q = dirMap[mDir][word];
-    LinkedList popped; popped.init();
+    char subWord[MAX_WORD];
+    strcpy_s(subWord, rooms[current_roomID].dir[mDir]);
+
+    int mIdx = get_mapIndex(mDir, subWord);
+    auto& Q = dirPQ[mDir][mIdx];
+
     int ret = 0;
-
+    LinkedList<int> popped;
     while (!Q.empty()) {
-        Data room = Q.top(); Q.pop();
+        auto data = Q.top(); Q.pop();
 
-        if (strcmp(rooms[room.mID].word, room.word) != 0) continue;
+        if (strcmp(rooms[data.mID].mWord, data.mWord) != 0) continue;
 
-        popped.push_back(room.mID);
-        if (room.mID != current_mID) {
-            ret = room.mID;
+        popped.push_back(data.mID);
+        if (data.mID != current_roomID) {
+            ret = data.mID;
             break;
         }
     }
     for (auto node = popped.head; node; node = node->next) {
-        int mID = node->value;
-        Q.push({ mID, rooms[mID].word });
-    }
-    if (ret != 0) { current_mID = ret; }
+        int mID = node->data;
+        Q.push({ mID, rooms[mID].mWord }); }
+    if (ret != 0) { current_roomID = ret; }
     return ret;
 }
 
-// 30,000
 void changeWord(char mWord[], char mChgWord[], int mChgLen[])
 {
-    int mID = roomMap[string(mWord)];
+    int mID = roomMap[mWord];
     addRoom(mID, mChgWord, mChgLen);
 }
 #endif
