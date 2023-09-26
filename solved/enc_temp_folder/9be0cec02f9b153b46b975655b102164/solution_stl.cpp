0e7ad483656642b6647ee5f4671ac4298a340285
nampluskr <nampluskr@gmail.com>
2023-09-27 01:12:41 +0900

    create

---
+++ solved/enc_temp_folder/9be0cec02f9b153b46b975655b102164/solution_stl.cpp
@@ -0,0 +1,45 @@
+#ifndef _CRT_SECURE_NO_WARNINGS
+#define _CRT_SECURE_NO_WARNINGS
+#endif
+
+#include <vector>
+#include <string>
+#include <cstring>
+using namespace std;
+
+#define MAX_STOCK_LEN 10
+#define MAX_STOCKS	100000
+
+struct Stock {
+	char mStocks[MAX_STOCK_LEN + 1];
+	int mPrice;
+};
+vector<Stock> stocks;
+int stockCnt;
+
+
+void init(int N, char mStocks[][MAX_STOCK_LEN + 1], int mPrices[])
+{
+	stocks.clear();	stocks.resize(N);
+	int stockCnt = N;
+
+	for (int i = 0; i < N; i++) {
+		strcpy(stocks[i].mStocks, mStocks[i]);
+		stocks[i].mPrice = mPrices[i];
+	}
+}
+
+void changePrices(char mFromStock[], char mToStock[], int mPriceDiff)
+{
+
+}
+
+int getPrice(char mStock[])
+{
+	return 0;
+}
+
+int getMostIncreasedStock(char mFromStock[], char mToStock[])
+{
+	return -1;
+}
