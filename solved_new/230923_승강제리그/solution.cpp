915885842ac0364c1f0dec25cf670532f00ea90e
nampluskr <nampluskr@gmail.com>
2023-10-20 14:00:54 +0900

    Create solution.cpp

---
+++ solved_new/230923_승강제리그/solution.cpp
@@ -0,0 +1,44 @@
+#include <vector>
+#include <set>
+using namespace std;
+
+struct Player {
+
+};
+vector<Player> players;
+int playerCnt;
+
+struct League {
+};
+vector<League> leagues;
+int leagueCnt;
+
+/////////////////////////////////////////////////////////////////////
+void init(int N, int L, int mAbility[])
+{
+    playerCnt = 0;
+    leagueCnt = 0;
+
+    players.clear();    players.resize(N);
+    leagues.clear();    leagues.resize(L);
+}
+
+
+
+int move()
+
+{
+
+    return 0;
+
+}
+
+
+
+int trade()
+
+{
+
+    return 0;
+
+}
