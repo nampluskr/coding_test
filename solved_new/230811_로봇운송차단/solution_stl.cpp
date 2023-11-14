93047a8e30368af287349fe45bc3e59a441acac9
nampluskr <nampluskr@gmail.com>
2023-11-14 20:17:08 +0900

    Create solution_stl.cpp

---
+++ solved_new/230811_로봇운송차단/solution_stl.cpp
@@ -0,0 +1,32 @@
+#include <vector>
+#include <queue>
+#include <unordered_map>
+using namespace std;
+
+#define MAX_N 20
+
+int map[MAX_N][MAX_N];
+int N;      // map size
+
+
+////////////////////////////////////////////////////////////////////
+void init(int _N, int mMap[MAX_N][MAX_N])
+{
+    N = _N;
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            map[i][j] = mMap[i][j];
+
+}
+
+int numberOfCandidate(int M, int mStructure[])
+{
+
+    return 0;
+}
+
+int maxBlockedRobots(int M, int mStructure[], int mDir)
+{
+
+    return 0;
+}
