b21991efd306bedaedda3d4e8f7c95e3183769e5
nampluskr <37031766+nampluskr@users.noreply.github.com>
2023-12-08 16:46:30 +0900

    Update graph_search.cpp

--- ref_new/dfs_bfs_graph/graph_search.cpp
+++ ref_new/dfs_bfs_graph/graph_search.cpp
@@ -1,5 +1,5 @@
-﻿#if 0
-// 그래프 노드 차례대로 순회 (상위 노드정보 저장 목적)
+#if 0
+// 그래프내 특정 노드 찾기 (찾으면 찾기 중단)
 #include <vector>       // 링크드 리스트
 #include <stack>        // DFS 구현
 #include <queue>        // BFS 구현
@@ -9,19 +9,19 @@ using namespace std;
 
 vector<int> nodes[N];   // 노드 연결정보 저장
 bool visited[N];        // 노드 방문여부 저장
-vector<int> path;       // 노드 방문순서 저장
+vector<int> history;       // 노드 방문순서 저장
 
-void print_path(const vector<int>& path) {
+void show(const vector<int>& vec) {
     int i;
-    for (i = 0; i < path.size() - 1; i++) { printf("[%d] - ", path[i]); }
-    printf("[%d]\n", path[i]);
+    for (i = 0; i < vec.size() - 1; i++) { printf("[%d] - ", vec[i]); }
+    printf("[%d]\n", vec[i]);
 }
 
 /////////////////////////////////////////////////////////////////////
 bool dfs1(int x, int target) {
     if (!visited[x]) {
-        visited[x] = 1;
-        path.push_back(x);
+        visited[x] = 1;         // 먼저 방문 처리
+        history.push_back(x);
 
         if (x == target) {
             printf("Found the target!\n");
@@ -33,7 +33,7 @@ bool dfs1(int x, int target) {
     return false;
 }
 
-bool dfs2(int start, int target) {
+void dfs2(int start, int target) {
     stack<int> S;
     S.push(start);
 
@@ -41,18 +41,17 @@ bool dfs2(int start, int target) {
         int x = S.top(); S.pop();
 
         if (!visited[x]) {
-            visited[x] = 1;
-            path.push_back(x);
+            visited[x] = true;  // 먼저 방문 처리
+            history.push_back(x);
 
             if (x == target) {
                 printf("Found the target!\n");
-                return true;
+                return;
             }
             for (int next : nodes[x])
                 S.push(next);
         }
     }
-    return false;
 }
 
 bool dfs3(int x, int target) {
@@ -62,34 +61,35 @@ bool dfs3(int x, int target) {
     }
     for (int nx : nodes[x])
         if (!visited[nx]) {
-            visited[nx] = true;
-            path.push_back(nx);
+            visited[nx] = true;     // 나중에 방문 처리
+            history.push_back(nx);
             if (dfs3(nx, target) == true) { return true; }
         }
     return false;
 }
 
-bool bfs(int start, int target) {
+void bfs(int start, int target) {
     queue<int> Q;
-    visited[start] = true;
-    path.push_back(start);
+    visited[start] = true;          // 시작점 방문 처리
+    history.push_back(start);
     Q.push(start);
 
     while (!Q.empty()) {
         int x = Q.front(); Q.pop();
 
-        if (x == target) {
-            printf(">> Found the target!\n");
-            return true;   
-        }
-        for (int nx : nodes[x])
-            if (!visited[nx]) {
-                visited[nx] = true;
-                path.push_back(nx);
-                Q.push(nx);
+        for (int nx : nodes[x]) {
+            if (visited[nx]) continue;
+
+            visited[nx] = true;     // 나중에 방문 처리
+            history.push_back(nx);
+
+            if (nx == target) {
+                printf(">> Found the target!\n");
+                return;
             }
+            Q.push(nx);
+        }
     }
-    return false;
 }
 
 
@@ -99,7 +99,7 @@ void init() {
         nodes[i].clear();   // 노드 연결정보 초기화
         visited[i] = 0;     // 노드 방문정보 초기화
     }
-    path.clear();           // 노드 방문순서 초기화
+    history.clear();           // 노드 방문순서 초기화
 
     nodes[0] = { 1, 2 };
     nodes[1] = { 0, 2, 4, 7 };
@@ -111,25 +111,25 @@ void init() {
     nodes[7] = { 1 };
 }
 
-int main() 
+int main()
 {
     init();
     dfs1(0, 4);
-    print_path(path);   // [0] - [1] - [2] - [3] - [5] - [4]
+    show(history);      // [0] - [1] - [2] - [3] - [5] - [4]
 
     init();
     dfs2(0, 4);
-    print_path(path);   // [0] - [2] - [3] - [5] - [1] - [7] - [4]
+    show(history);      // [0] - [2] - [3] - [5] - [1] - [7] - [4]
 
     init();
-    visited[0] = true;
-    path.push_back(0);
+    visited[0] = true;  // 시작점 방문 처리
+    history.push_back(0);
     dfs3(0, 4);
-    print_path(path);   // [0] - [1] - [2] - [3] - [5] - [4]
+    show(history);      // [0] - [1] - [2] - [3] - [5] - [4]
 
     init();
     bfs(0, 4);
-    print_path(path);   // [0] - [1] - [2] - [4] - [7] - [3]
+    show(history);      // [0] - [1] - [2] - [4] - [7] - [3]
 
     return 0;
 }
